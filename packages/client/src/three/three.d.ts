d5a9de78026d4a95970b4ae41e7b2c6bbf571190
Austin Wasinger <austinwasinger@gmail.com>
2026-07-21 23:31:03 -0600

    Declare SkeletonUtils module

--- packages/client/src/three/three.d.ts
+++ packages/client/src/three/three.d.ts
@@ -5,3 +5,7 @@ declare module "three/examples/jsm/loaders/FBXLoader.js" {
     load(url: string, onLoad: (model: unknown) => void, onProgress?: undefined, onError?: (error: unknown) => void): void;
   }
 }
+
+declare module "three/examples/jsm/utils/SkeletonUtils.js" {
+  export function clone(source: unknown): unknown;
+}
